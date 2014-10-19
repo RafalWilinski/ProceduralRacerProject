8e23a520a6b2c822dbc89ddeff7453178817ca5d
Rafal Wilinski <tho.ornee@gmail.com>
2014-10-19 18:32:14 +0200

    Awesome graphics improvements
    Theme toolkit
    Obj importer, relatime theme editor etcetctec

--- Assets/Editor/ThemeManagerEditor.cs
+++ Assets/Editor/ThemeManagerEditor.cs
@@ -5,14 +5,63 @@ using UnityEditor;
 [CustomEditor(typeof(ThemeManager))]
 public class ThemeManagerEditor : Editor {
 
-public float sizeMultiplier;
+	public float sizeMultiplier;
+	public string themeName;
+	public Color lightsColor;
+	public Color materialColor;
+	public Color backgroundColor;
+	public Color ambientColor = new Color(0.2f, 0.2f, 0.2f);
+	public bool isRealtime;
+	public bool showThemeCreator;
+
 	public override void OnInspectorGUI()
 	{
 		DrawDefaultInspector();
 
+		EditorGUILayout.Space();
+		EditorGUILayout.LabelField("Theme Toolbag");
+
 		ThemeManager myScript = (ThemeManager)target;
 		if(GUILayout.Button("Next Theme")) {
 			myScript.NextTheme();
+		}
+
+		if(GUILayout.Button("Next Lerp to Theme")) {
+			myScript.NextThemeLerp();
+		}
+
+		if(GUILayout.Button("Update Current Theme")) {
+			myScript.UpdateCurrentTheme();
+		}
+
+		EditorGUILayout.Space();
+		showThemeCreator = EditorGUILayout.Foldout(showThemeCreator, "Theme Creation");
+		if(showThemeCreator) {
+			themeName = EditorGUILayout.TextField("Name: ",themeName);
+			lightsColor = EditorGUILayout.ColorField("Lights Color: ", lightsColor);
+			materialColor = EditorGUILayout.ColorField("Material Color: ", materialColor);
+			backgroundColor = EditorGUILayout.ColorField("Background Color: ", backgroundColor);
+			ambientColor = EditorGUILayout.ColorField("Ambient Color: ", ambientColor);
+
+			isRealtime = EditorGUILayout.Toggle("Realtime edit", isRealtime);
+
+			if(GUILayout.Button("Save this Theme")) {
+				myScript.currentThemeIndex = myScript.themes.Count;
+				myScript.AddOrChangeTheme(new ThemeManager.Theme(themeName, lightsColor, materialColor, backgroundColor, ambientColor));
+				isRealtime = false;
+			}
+
+			if(GUILayout.Button("Load theme from current settings")) {
+				lightsColor = myScript.light.color;
+				materialColor = myScript.mat.color;
+				backgroundColor = RenderSettings.fogColor;
+				ambientColor = RenderSettings.ambientLight;
+			}
+
+		}
+
+		if(isRealtime) {
+			myScript.SetTheme(new ThemeManager.Theme(themeName, lightsColor, materialColor, backgroundColor, ambientColor));
 		}
 	}
 }
--- Assets/Scripts/ThemeManager.cs
+++ Assets/Scripts/ThemeManager.cs
@@ -10,8 +10,8 @@ public class ThemeManager : MonoBehaviour {
 	public Light light;
 	public Camera cam;
 	public Material mat;
-
-	private int currentThemeIndex;
+	public float lerpSpeed;
+	public int currentThemeIndex;
 
 	[Serializable]
 	public class Theme {
@@ -20,11 +20,24 @@ public class ThemeManager : MonoBehaviour {
 		public Color materialColor;
 		public Color backgroundColor;
 		public Color ambientColor = new Color(0.2f, 0.2f, 0.2f);
+
+		public Theme(string n, Color l, Color m, Color b, Color a) { name = n; lightsColor = l; materialColor = m; backgroundColor = b; ambientColor = a; }
+	}
+
+	void Update() {
+		if(Input.GetKeyUp(KeyCode.N)) {
+			NextThemeLerp();
+		}
 	}
 
 	public void SwitchTo (int i) {
 		currentThemeIndex = i;
 		SetTheme(themes[i]);
+	}
+
+	public void LerpTo (int i) {
+		currentThemeIndex = i;
+		StartCoroutine(LerpToTheme(themes[i]));
 	}
 
 	public void NextTheme() {
@@ -33,12 +46,48 @@ public class ThemeManager : MonoBehaviour {
 		SwitchTo(currentThemeIndex);
 	}
 
-	private void SetTheme(Theme t) {
-		Debug.Log("Switching theme to: "+t.name);
+	public void NextThemeLerp() {
+		if(currentThemeIndex + 1 >= themes.Count) currentThemeIndex = 0;
+		else currentThemeIndex++;
+		LerpTo(currentThemeIndex);
+	}
+
+	public void SetTheme(Theme t) {
 		light.color = t.lightsColor;
 		cam.backgroundColor = t.backgroundColor;
 		mat.color = t.materialColor;
 		RenderSettings.fogColor = t.backgroundColor;
 		RenderSettings.ambientLight = t.ambientColor;
+	}
+
+	public void UpdateCurrentTheme() {
+		SetTheme(themes[currentThemeIndex]);
+	}
+
+	public void AddOrChangeTheme(Theme t) {
+		for(int i = 0; i<themes.Count; i++) {
+			if(themes[i].name == t.name) {
+				themes[i] = t;
+				return;
+			}
+		}
+
+		themes.Add(t);
+	}
+
+	private IEnumerator LerpToTheme(Theme t) {
+		Debug.Log("Lerping theme to: "+t.name);
+		float elapsedTime = 0f;
+		while(lerpSpeed >= elapsedTime) {
+			light.color = Color.Lerp(light.color, t.lightsColor, Time.deltaTime);
+			cam.backgroundColor = Color.Lerp(cam.backgroundColor, t.backgroundColor, Time.deltaTime);
+			mat.color = Color.Lerp(mat.color, t.materialColor, Time.deltaTime);
+			RenderSettings.fogColor = Color.Lerp(RenderSettings.fogColor, t.backgroundColor, Time.deltaTime);
+			RenderSettings.ambientLight = Color.Lerp(RenderSettings.ambientLight, t.ambientColor, Time.deltaTime);
+			elapsedTime += Time.deltaTime;
+			yield return new WaitForSeconds(Time.deltaTime);
+		}
+		SetTheme(t);
+		Debug.Log("Lerp complete!");
 	}
 }
