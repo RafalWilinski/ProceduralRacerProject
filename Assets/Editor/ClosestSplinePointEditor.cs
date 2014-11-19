2bc8c343e1c28981430495fd63d2a0f5f2f41fef
Rafal Wilinski <tho.ornee@gmail.com>
2014-11-19 23:32:06 +0100

    Sync

---
+++ Assets/Editor/ClosestSplinePointEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(ClosestSplinePoint))]
+public class ClosestSplinePointEditor : Editor {
+
+	public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ClosestSplinePoint myScript = (ClosestSplinePoint)target;
+        if(GUILayout.Button("Simulate!")) {
+            myScript.Simulate();
+        }
+    }
+}
